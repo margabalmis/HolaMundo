4c19e5500020b69081332097ef0e1fd28af5c4d4
alumno <alumno@A302PC22>
2021-09-23 12:02:52 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundo/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace HolaMundo
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string cadena;
+
+            cadena = NombreTexto.Text;
+            MensajeTextBlock.Text = "Hola " + cadena + "!!!";
+
+            MensajeTextBlock.Foreground = Brushes.DarkCyan;
+        }
+    }
+}
